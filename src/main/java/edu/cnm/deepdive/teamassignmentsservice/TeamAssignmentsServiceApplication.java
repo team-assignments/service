94fd2df3dc44b431df73240dd1fb2f47d529e85f
Marshall Eldridge <l2marshall@gmail.com>
2021-07-08 09:11:37 -0600

    initial commit.

---
+++ src/main/java/edu/cnm/deepdive/teamassignmentsservice/TeamAssignmentsServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.teamassignmentsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamAssignmentsServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TeamAssignmentsServiceApplication.class, args);
+  }
+
+}
